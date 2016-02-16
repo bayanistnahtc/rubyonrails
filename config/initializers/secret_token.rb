30c66f073ba8bedd6a33854f09aad0edd2c35b6d
bayanistnahtc <bayanistnahtc@gmail.com>
2016-02-16 13:08:34 +0300

    Depod Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deport::Application.config.secret_key_base = '536d0bb7e61144740ca47e7f3b0aa064c3556eba52ae77c556c17cc04495111b2ae4d8390e06225f90eebe709d7d2ad655c05b8c358e4f85a48b4b5c581c77a5'
